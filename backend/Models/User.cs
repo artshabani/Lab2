b4a7b73178bc76258524decb41d2a4974eaa2a8a
artshabani2002 <artshabani2002@gmail.com>
2023-04-15 18:29:16 +0200

    user model

---
+++ backend/Models/User.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    private string _passwordHash;
+
+    [Required]
+    public string Password
+    {
+        get { return _passwordHash; }
+        set { _passwordHash = BCrypt.Net.BCrypt.HashPassword(value); }
+    }
+}
+
+}
