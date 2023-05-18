1b0996c76a976fec7e356deb7a5af59d81b72d5a
IdeaXhafa <ix51808@ubt-uni.net>
2023-05-18 15:07:09 +0200

    test

--- frontend/src/components/Movies/MovieCard.js
+++ frontend/src/components/Movies/MovieCard.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../ComponentsCSS/MovieCard.css';
 
 function MovieCard({ movie }) {
   return (
     <div className="movie-card">
       <Link to={`/movies/${movie.id}`}>
-        {/* <img src={movie.imageUrl} alt={movie.title} />
-        <h3>{movie.title}</h3> */}
-        <div class="frem">
-          <div class="center-cant">
-            <img src={movie.imageUrl} alt={movie.title} />
-            <div class="row">
-              <p>{movie.title}</p>
-              <h2>{movie.viewCount} views</h2>
-            </div>
-          </div>
-        </div>
+        <img src={movie.imageUrl} alt={movie.title} />
+        <h3>{movie.title}</h3>
       </Link>
       <p>{movie.description}</p>
       <b>
