22a9349185369c4af6933f597ed43bf976a3f2b0
IdeaXhafa <ix51808@ubt-uni.net>
2023-06-01 23:17:27 +0200

    fixed login signup dishka

--- frontend/src/components/auth/Login/Login.js
+++ frontend/src/components/auth/Login/Login.js
@@ -1,141 +1,137 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { setUser } from "../../../redux/actions";
 
-export class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: null,
-            password: null,
-            login: false,
-            store: null,
-            rememberMe: false
-        }
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const state = useSelector(state => state);
+
+  useEffect (() => {
+    if(state.user){
+        navigate('/')
     }
+  },[state.user])
 
-    componentDidMount() {
-        this.storeCollector()
-    }
+  const handleRememberMeChange = (event) => {
+    setRememberMe(event.target.checked);
+  };
 
-    storeCollector() {
-        let store = JSON.parse(localStorage.getItem('login'));
-        if (store && store.login) {
-            this.setState({ login: true, store: store })
-        }
-    }
-
-    login() {
-        const email = document.getElementById('email_field').value;
-        const password = document.getElementById('password_field').value;
-
-        const requestBody = {
-            email: email,
-            password: password
-        };
-
-        fetch('http://localhost:5000/api/account/login', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/json'
-            },
-            body: JSON.stringify(requestBody)
-        }).then((response) => {
-            response.json().then((result) => {
-                console.warn("result", result);
-                localStorage.setItem('token', result.token);
-                this.storeCollector();
-            })
-
-
-            if (response.ok) {
-                console.log('Login successful');
-            } else {
-                console.error('Login failed');
-            }
-        })
-    }
-
-    post() {
-        let token = "Bearer" + localStorage.getItem('token');
-        fetch('http://localhost:5000/api/movies', {
-            method: "POST",
-            headers: {
-                'Authorization': token
-            },
-            body: JSON.stringify(this.state)
-        }).then((response) => {
-            response.json().then((result) => {
-                this.setState({
-                    response: result.message
-                })
-                console.warn("result", result);
-            })
-        })
-    }
-
-    handleRememberMeChange = (event) => {
-        this.setState({ rememberMe: event.target.checked });
+  const login = () => {
+    const requestBody = {
+      email: email,
+      password: password,
     };
 
-    render() {
-        return (
-            <>
-                {/* <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <form>
-                                <h3 style={{ color: 'black', textAlign: 'left' }}>Login</h3>
-                                <h4 style={{ color: 'black', textAlign: 'left' }}>Use an existing account to log in.</h4>
-                                <br />
-                                <div className="form-group">
-                                    <label htmlFor="email" style={{ textAlign: 'left' }}>Email</label>
-                                    <input type="email" className="form-control" id="email_field" placeholder="Enter email" />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password" style={{ textAlign: 'left' }}>Password</label>
-                                    <input type="password" className="form-control" id="password_field" placeholder="Enter password" />
-                                </div>
-                                <div className="form-group form-check">
-                                    <input type="checkbox" className="form-check-input" id="rememberMe" checked={this.state.rememberMe} onChange={this.handleRememberMeChange} />
-                                    <label className="form-check-label" htmlFor="rememberMe" style={{ textAlign: 'left' }}>Remember Me</label>
-                                </div>
-                                <button type="submit" onClick={() => { this.login() }} className="btn btn-primary">Login</button>
-                                <br />
-                                <br />
-                                <div style={{ display: 'flex' , justifyContent: 'center'}}>
-                                    <h6 style={{ marginRight: '5px' }}>Don't have an account?</h6>
-                                    <Link to="/signup">Sign up</Link>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div> */}
+    fetch("http://localhost:5000/api/account/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then((response) => {
+        response.json().then((result) => {
+          localStorage.setItem("token", result.token);
+          dispatch(setUser(result))
+        });
 
-                <div className="center" style={{ alignItems: 'center' }}>
-                    <h1>Log In</h1>
+        if (response.ok) {
+          console.log("Login successful");
+        } else {
+          console.error("Login failed");
+        }
+      });
+  };
 
-                    Email:
-                    <div className="txt_field">
-                        <input type="text" id="email_field" /><br />
-                    </div>
-                    Password:
-                    <div className="txt_field">
-                        <input type="password" id="password_field" /> <br />
-                    </div>
-
-                    <div className="button">
-                        <button onClick={() => { this.login() }} className="btn btn-primary">Log in</button>
-                    </div>
-                </div>
-            </>
-        )
-    }
-}
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <form>
+              <h3 style={{ color: "black", textAlign: "left" }}>Login</h3>
+              <h4 style={{ color: "black", textAlign: "left" }}>
+                Use an existing account to log in.
+              </h4>
+              <br />
+              <div className="form-group">
+                <label htmlFor="email" style={{ textAlign: "left" }}>
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" style={{ textAlign: "left" }}>
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="rememberMe"
+                  checked={rememberMe}
+                  onChange={handleRememberMeChange}
+                />
+                <label
+                  className="form-check-label"
+                  htmlFor="rememberMe"
+                  style={{ textAlign: "left" }}
+                >
+                  Remember Me
+                </label>
+              </div>
+              <button
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  login();
+                }}
+                className="btn btn-primary"
+              >
+                Login
+              </button>
+              <br />
+              <br />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <h6 style={{ marginRight: "5px" }}>Don't have an account?</h6>
+                <Link to="/signup">Sign up</Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
--- frontend/src/components/auth/Signup/Signup.js
+++ frontend/src/components/auth/Signup/Signup.js
@@ -1,37 +1,25 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from '../../../redux/actions';
 
-export class Signup extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: null,
-      username: null,
-      email: null,
-      password: null,
-      signup: false,
-      store: null,
-    };
-  }
+const Signup = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const state = useSelector(state => state);
 
-  componentDidMount() {
-    this.storeCollector();
-  }
-
-  storeCollector() {
-    let store = JSON.parse(localStorage.getItem('signup'));
-    if (store && store.signup) {
-      this.setState({ signup: true, store: store });
+  useEffect (() => {
+    if(state.user){
+        navigate('/')
     }
-  }
+  },[state.user])
 
-  signup() {
-    const name = document.getElementById('name_field').value;
-    const username = document.getElementById('username_field').value;
-    const email = document.getElementById('email_field').value;
-    const password = document.getElementById('password_field').value;
-
+  const signup = () => {
     const requestBody = {
       name: name,
       username: username,
@@ -46,93 +34,94 @@ export class Signup extends Component {
         accept: 'application/json',
       },
       body: JSON.stringify(requestBody),
-    }).then(response => {
-      response.json().then(result => {
-        console.warn('result', result);
+    }).then((response) => {
+      response.json().then((result) => {
         localStorage.setItem('token', result.token);
-        //this.storeCollector()
+        dispatch(setUser(result))
       });
     });
-  }
+  };
 
-  render() {
-    return (
-      <>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <form>
-                <h3 style={{ color: 'black', textAlign: 'left' }}>Sign up</h3>
-                <h4 style={{ color: 'black', textAlign: 'left' }}>Create a new account.</h4>
-                <br />
-                <div className="form-group">
-                  <label htmlFor="email" style={{ textAlign: 'left' }}>Name</label>
-                  <input type="email" className="form-control" id="name_field" placeholder="Enter name" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email" style={{ textAlign: 'left' }}>Username</label>
-                  <input type="email" className="form-control" id="username_field" placeholder="Enter username" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email" style={{ textAlign: 'left' }}>Email</label>
-                  <input type="email" className="form-control" id="email_field" placeholder="Enter email" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password" style={{ textAlign: 'left' }}>Password</label>
-                  <input type="password" className="form-control" id="password_field" placeholder="Enter password" />
-                </div>
-                <div className="form-group form-check">
-                  <input type="checkbox" className="form-check-input" id="rememberMe" checked={this.state.rememberMe} onChange={this.handleRememberMeChange} />
-                  <label className="form-check-label" htmlFor="rememberMe" style={{ textAlign: 'left' }}>Remember Me</label>
-                </div>
-                <button type="submit" onClick={() => {this.signup();}} className="btn btn-primary">Signup</button>
-                <br />
-                <br />
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <h6 style={{ marginRight: '5px' }}>Already have an account?</h6>
-                  <Link to="/login">Log in</Link>
-                </div>
-              </form>
-            </div>
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <form>
+              <h3 style={{ color: 'black', textAlign: 'left' }}>Sign up</h3>
+              <h4 style={{ color: 'black', textAlign: 'left' }}>Create a new account.</h4>
+              <br />
+              <div className="form-group">
+                <label htmlFor="email" style={{ textAlign: 'left' }}>
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" style={{ textAlign: 'left' }}>
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" style={{ textAlign: 'left' }}>
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" style={{ textAlign: 'left' }}>
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  signup();
+                }}
+                className="btn btn-primary"
+              >
+                Signup
+              </button>
+              <br />
+              <br />
+            </form>
           </div>
         </div>
-      </>
-      // <div className="center" style={{ alignItems: 'center' }}>
-      //   <h1>Sign up</h1>
-      //   Name:
-      //   <div className="txt_field">
-      //     <input type="text" id="name_field" /> <br />
-      //   </div>
-      //   Username:
-      //   <div className="txt_field">
-      //     <input type="text" id="username_field" /> <br />
-      //   </div>
-      //   Email:
-      //   <div className="txt_field">
-      //     <input type="text" id="email_field" /> <br />
-      //   </div>
-      //   Password:
-      //   <div className="txt_field">
-      //     <input type="password" id="password_field" /> <br />
-      //   </div>
-      //   <div className="button">
-      //     <button
-      //       onClick={() => {
-      //         this.signup();
-      //       }}
-      //       className="btn btn-primary"
-      //     >
-      //       Sign up
-      //     </button>
-      //   </div>
-      // </div>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
 export default Signup;
