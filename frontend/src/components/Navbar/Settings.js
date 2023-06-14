2574d01363ba19baec8d6a8e0c8fcd2c24021620
MedinaSekiraqa <102260547+MedinaSekiraqa@users.noreply.github.com>
2023-06-14 23:25:55 +0200

    Setings and privacy page

--- frontend/src/components/Navbar/Settings.js
+++ frontend/src/components/Navbar/Settings.js
@@ -1,10 +1,62 @@
-import React from "react"
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
-function Settings(){
+const PrivacySettingsPage = () => {
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const [autoPlayEnabled, setAutoPlayEnabled] = useState(false);
 
-    return(
-        <h1>medina</h1>
-    )
-}
+  const handleNotificationsChange = () => {
+    setNotificationsEnabled(!notificationsEnabled);
+  };
 
-export default Settings;
+  const handleAutoPlayChange = () => {
+    setAutoPlayEnabled(!autoPlayEnabled);
+  };
+
+  const handleSaveSettings = (event) => {
+    event.preventDefault();
+    // Add logic to save the settings
+    console.log('Settings saved!');
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="card-title text-center mb-4">Privacy and Settings</h1>
+              <Form>
+                <Form.Group controlId="formNotificationsCheckbox">
+                  <Form.Check
+                    type="switch"
+                    label="Enable notifications"
+                    checked={notificationsEnabled}
+                    onChange={handleNotificationsChange}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formAutoPlayCheckbox">
+                  <Form.Check
+                    type="switch"
+                    label="Enable autoplay"
+                    checked={autoPlayEnabled}
+                    onChange={handleAutoPlayChange}
+                  />
+                </Form.Group>
+
+                <div className="d-grid">
+                  <Button variant="primary" onClick={handleSaveSettings} size="lg">
+                    Save
+                  </Button>
+                </div>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacySettingsPage;
